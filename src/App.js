fa1041910534721bc340d1e57957834e67dff58a
anhpt6852 <anhpt6852@seta-international.vn>
2021-02-25 13:14:27 +0700

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import data from "./data.json";
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState(data);
+  const [userInput, setUserInput ] = useState('');
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map((task) => {
+      return task.id === Number(id)
+        ? { ...task, complete: !task.complete }
+        : { ...task, complete: task.complete };
+    });
+    setToDoList(mapped);
+  };
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter((task) => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  };
+
+  const handleChange = (event) => {
+      setUserInput(event.target.value)
+  }
+  const addTask = (userInput) => {
+    let newTodo = [
+      ...toDoList,
+      { id: toDoList.length + 1, task: userInput, complete: false }
+    ];
+    setToDoList(newTodo);
+  };
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      addTask(userInput);
+      setUserInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <form className = 'add-form' onSubmit={handleSubmit}>
+            <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+            <button className= 'add-button'>Submit</button>
+        </form>
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleFilter={handleFilter}
+      />
+
     </div>
   );
 }
